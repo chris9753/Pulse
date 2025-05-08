784b15431ce0f3ef597274365a73558ec4eea0ac
chris9753 <mr.chrisdasilver@gmail.com>
2025-05-08 23:53:55 +0545

    feat: update new campaign page UI layout

--- app/campaigns/new/page.tsx
+++ app/campaigns/new/page.tsx
@@ -442,6 +442,7 @@ export default function NewCampaignPage() {
                       onSave={(html) => setContent(html)}
                       onExportToHtml={handleExportToHtml}
                       className="w-full h-full"
+                      autoSave={true}
                     />
                   )}
                 </div>
